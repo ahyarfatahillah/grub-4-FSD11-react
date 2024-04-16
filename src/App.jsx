5415da9817c670bf8b5107bbe09ac59851f31391
fadhlil18 <fadhlilazfi@gmail.com>
2024-04-16 22:08:49 +0700

    migrasi login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-
+import Login from "./pages/Login"
 function App() {
 
   return (
-    <>
-    <h1 className='bg-blue-500'>Kelompok-4</h1>
-    </>
+<Login></Login>
   )
 }
 
