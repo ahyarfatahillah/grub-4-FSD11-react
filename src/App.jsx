655e61a3496d87048eeabdf0d11580d652df8eea
fadhlil18 <fadhlilazfi@gmail.com>
2024-04-17 07:26:50 +0700

    migrasi signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-
+import Signup from "./pages/Signup"
 function App() {
 
   return (
-    <>
-    <h1 className='bg-blue-500'>Kelompok-4</h1>
-    </>
-  )
+  <Signup/> 
+)
 }
 
 export default App
