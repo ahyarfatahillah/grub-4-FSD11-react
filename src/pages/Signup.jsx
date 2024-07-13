02832ad79fc1a169c57a993f286cb8ea66ee5e89
fadhlil18 <fadhlilazfi@gmail.com>
2024-07-14 04:09:03 +0700

    link signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,51 +5,51 @@ import React, { useState } from 'react';
 const Signup = () => {
     const navigate = useNavigate();
     const [user, setUser] = useState({
-      email: '',
-      password: ''
+        email: '',
+        password: ''
     });
     const [error, setError] = useState('');
-  
+
     const emailHandler = (e) => {
-      setUser({
-        ...user,
-        email: e.target.value
-      })
+        setUser({
+            ...user,
+            email: e.target.value
+        })
     }
 
     const phoneHandler = (e) => {
         setUser({
-          ...user,
-          phone: e.target.value
+            ...user,
+            phone: e.target.value
         })
-      }
-    
-    const passwordHandler = (e) => {
-      setUser({
-        ...user,
-        password: e.target.value
-      })
     }
-  
+
+    const passwordHandler = (e) => {
+        setUser({
+            ...user,
+            password: e.target.value
+        })
+    }
+
     const submitHandler = async (e) => {
-      e.preventDefault();
-      const port = import.meta.env.VITE_API_PORT
-      const url = `http://localhost:${port}/api/users `;
-      const result = await fetch(url, {
-        body: JSON.stringify(user),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'POST'
-      });
-      const json = await result.json()
-      console.log(json, user)
-      if (json.message) {
-        setError(json.message);
-      } else {
-        alert('Registration Successful');
-        navigate('/login');
-      }
+        e.preventDefault();
+        const port = import.meta.env.VITE_API_PORT
+        const url = `http://localhost:${port}/api/users `;
+        const result = await fetch(url, {
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+        const json = await result.json()
+        console.log(json, user)
+        if (json.message) {
+            setError(json.message);
+        } else {
+            alert('Registration Successful');
+            navigate('/login');
+        }
     }
 
     return (
@@ -72,7 +72,7 @@ const Signup = () => {
                         The world is your greatest teacher.
                     </p>
                 </div>
-                <form className="form-login" action="#" method="post" onSubmit={ submitHandler }>
+                <form className="form-login" action="#" method="post" onSubmit={submitHandler}>
                     <label htmlFor="email" className="m-1 text-xs md:text-base lg:text-lg">
                         Email
                     </label>
@@ -84,7 +84,7 @@ const Signup = () => {
                         required=""
                         autofocus=""
                         value={user.email}
-                        onChange={ emailHandler }
+                        onChange={emailHandler}
                     />
                     <label htmlFor="Phone" className="m-1 text-xs md:text-base lg:text-lg">
                         Phone Number
@@ -98,7 +98,7 @@ const Signup = () => {
                         required=""
                         autofocus=""
                         value={user.phone}
-                        onChange={ phoneHandler }
+                        onChange={phoneHandler}
                     />
                     <label htmlFor="Password" className="m-1 md:text-base text-xs lg:text-lg">
                         Password
@@ -113,35 +113,35 @@ const Signup = () => {
                         pattern=".{8,}"
                         title="Eight or more characters"
                         value={user.password}
-                        onChange={ passwordHandler }
+                        onChange={passwordHandler}
                     />
-                    <Link to="../Login"><button className="btn text-xs lg:text-base md:text-sm" type="submit" onClick={ submitHandler }>
+                    <Link to="../Login"><button className="btn text-xs lg:text-base md:text-sm" type="submit" onClick={submitHandler}>
                         Sign up
                     </button></Link>
                 </form>
                 <div className="or-container">
                     <h3>or</h3>
-                    <button className="btng text-xs lg:text-base md:text-sm" type="submit">
+                    <Link to="/"><button className="btng text-xs lg:text-base md:text-sm" type="submit">
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png"
                             alt="#"
                         />
                         Continue With Facebook
-                    </button>
-                    <button className="btng text-xs lg:text-base md:text-sm" type="submit">
+                    </button></Link>
+                    <Link to="/"><button className="btng text-xs lg:text-base md:text-sm" type="submit">
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1024px-Google_%22G%22_logo.svg.png"
                             alt="#"
                         />
                         Continue with Google
-                    </button>
-                    <button className="btng text-xs lg:text-base md:text-sm" type="submit">
+                    </button></Link>
+                    <Link to="/"><button className="btng text-xs lg:text-base md:text-sm" type="submit">
                         <img
                             src="https://help.apple.com/assets/6362E41904F57C36D47F1246/6362E41E04F57C36D47F1254/en_US/cfef5ce601689564e0a39b4773f20815.png"
                             alt="#"
                         />
                         Continue with Apple
-                    </button>
+                    </button></Link>
                 </div>
             </div>
             <div className="right-img form-img ">
