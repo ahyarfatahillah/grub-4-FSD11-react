d62f9bd1dea2a8a4b73b23fb55e3af6ea0307245
rizalherlangga <rizsut03@gmail.com>
2024-07-14 11:31:50 +0700

    fix mapping detail

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,19 +1,32 @@
-import AddressEllipsis from "./Atoms/AddressEllipsis"
+import AddressEllipsis from "./Atoms/AddressEllipsis";
 
-const Maps = ({property}) => {
+const Maps = ({ property }) => {
+    const { lat, lon } = property; // Ambil lat dan lon dari data property
+    const mapSrc = `https://maps.google.com/maps?q=${lat},${lon}&hl=es;z=14&output=embed`;
+
     return (
         <div>
             <div className="maps py-[25px]">
                 <h2 className="font-bold text-[25px]">Where you'll be</h2>
-                <br/>
-                <AddressEllipsis address={property.address} country={property.country} className="font-bold text-2xl"/>
-                <br/>
-                <iframe src="https://maps.google.com/maps?q=10.305385,77.923029&hl=es;z=14&amp;output=embed" width="100%" height="450rem" className="border rounded-xl" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <div className="mt-[25px]"><a href=""><b>Show more</b></a></div>
+                <br />
+                <AddressEllipsis address={property.address} country={property.country} className="font-bold text-2xl" />
+                <br />
+                <iframe
+                    src={mapSrc} // Gunakan variabel mapSrc
+                    width="100%"
+                    height="450rem"
+                    className="border rounded-xl"
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+                <div className="mt-[25px]">
+                    <a href=""><b>Show more</b></a>
+                </div>
             </div>
-            <div className="border-b border-gray-300 pt-5 pb-5" ></div>
+            <div className="border-b border-gray-300 pt-5 pb-5"></div>
         </div>
-    )
-}
+    );
+};
 
-export default Maps
+export default Maps;
